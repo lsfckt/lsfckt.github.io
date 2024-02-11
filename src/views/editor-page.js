94a44b758f1362ec225cb0fe1f5ba90531c3d132
lsfckt <132505560+lsfckt@users.noreply.github.com>
2024-02-11 17:26:38 +0200

    title

--- src/views/editor-page.js
+++ src/views/editor-page.js
@@ -192,10 +192,11 @@ export async function editorPage(ctx) {
         e.preventDefault();
 
         const data = getFormData(e.target);
+        const title = data.title;
         const optionEl = document.querySelector('select').selectedOptions[0];
         const option = optionEl.innerText;
 
-        if (!data.title || option === "All categories") {
+        if (!title || option === "All categories") {
             alert('All fields are required! Please try again.');
             return;
         }
